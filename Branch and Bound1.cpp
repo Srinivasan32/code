28c69991d2cca1e5fa619a1124d0dc00738f30fb
Srinivasan32 <97144071+Srinivasan32@users.noreply.github.com>
2022-01-18 10:31:29 +0700

    Add files via upload

---
+++ Branch and Bound1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h >
+# include <string.h>
+# define N8
+void printSolution(int board[N][N])
+
+for ( int i - 0 ; 1 4 ; it + )
+for (int ]= 0 ; 1 < 4 ; j++ )
+Printf ( "12d", bound []LI);
+Print+ ( " In " );
+
+bool is Safe ( int slow , cal , slash Code [ N ] [ N ] , blackslash [ ] The
+bool rowlookup [], boal slash Code lookup [],
+bool back slash ( ode look up [] )
+if ( slash Code Lookup [slash Code [ now ]Ecal]] 11
+backslash Code Lookup [black slash (ode [Flow] [col]] 11
+nowLookup [low
+return false ;
+
+return true ;
+bool Solve NQueensUtil( int board [N][N], int cal,
+int slash Code [NJ[ N], in+ backslash (ode [NJ[N],
+boa TowLookup [NJ, bool slash Code Lookup CJ,
+boad blackslash Code Lookup [])
+
